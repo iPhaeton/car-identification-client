1f4c1ffe9de46c572b92984856a1ff3ab8e65e98
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-01-17 14:55:46 +0300

    Add Procfile

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
+  console.log('req')
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log('Server started', process.env.PORT || 8080)
 });
