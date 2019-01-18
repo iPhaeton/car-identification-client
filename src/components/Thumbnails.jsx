466a83b35d0f6c5776084479296e084ad77978bc
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-01-18 14:43:50 +0300

    Hide scrollbar on thumbnails

--- src/components/Thumbnails.jsx
+++ src/components/Thumbnails.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Thumbnail from './Thumbnail';
 import {withHorizontalScroll} from "../enhancers/withHorizontalScroll";
+import '../css/Thumbnails.css';
 
 class Thumbnails extends React.Component {
   render() {
     const {thumbnails, onThumbClick, onWheel} = this.props;
 
     return (
-      <div style={{width: '100%', height: 150, overflowX: 'scroll', overflowY: 'hidden', whiteSpace: 'nowrap'}} onWheel={onWheel}>
+      <div className="Thumbnails" onWheel={onWheel}>
         {thumbnails.map((th, i) => <Thumbnail thumbnail={th} key={i} onClick={onThumbClick} />)}
       </div>
     );
