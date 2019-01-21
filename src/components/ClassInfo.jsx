3df8714c82ea1fa05b02a0db92cac968bfa24bf7
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-01-21 14:07:11 +0300

    Can show hits for classnames

--- src/components/ClassInfo.jsx
+++ src/components/ClassInfo.jsx
@@ -13,7 +13,28 @@ const ClassInfo = props => {
     >
       <div style={{width: '60%', position: 'relative', display: 'flex', alignItems: 'center'}}>
         <div style={{height: '80%', width: `${percents}%`, backgroundColor: 'green'}}/>
-        <div style={{position: 'absolute', top: 0, left: 10, width: '100%', height: '100%', display: 'flex', alignItems: 'center'}}>{name}</div>
+        <div
+          title={name}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 10,
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <div style={{
+            textAlign: 'left',
+            width: '100%',
+            textOverflow: 'ellipsis',
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+          }}>
+            {name}
+          </div>
+        </div>
       </div>
       <div style={{width: '40%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>{percents.toFixed(3)}%</div>
     </div>
